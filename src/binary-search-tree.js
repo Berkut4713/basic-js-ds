e6d8c1484e2b3a2e13d30bc4db4f4254b9c30cf1
Berkut4713 <av.vecherov@gmail.com>
2022-10-16 16:38:47 +0300

    feat: add root

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,11 +8,13 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.rootValue = null;
   }
 
+  root() {
+    return this.rootValue;
+  }
   add(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
